3958722e47750c2103d8bd5437c404103098915d
wushijie <wushijie@melot.cn>
2023-08-10 16:35:31 +0800

    add: global style

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import '~/assets/root.css'
+import '~/assets/common.css'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
