5498a897f6257f5251e0af497d6e570bdc058bab
wushijie <wushijie@melot.cn>
2023-08-21 18:15:01 +0800

    Updates

---
+++ assets/List-e1301a9e.js
@@ -0,0 +1,1 @@
+import{r as t,j as r}from"./index-973a61f4.js";import{r as o}from"./request-8d1910c0.js";const n=()=>(t.useEffect(()=>{(async()=>{const s=await o("/list",null,"post",null);console.log(s)})()},[]),r.jsx("div",{children:"列表"}));export{n as default};
