24a802734d04085352db1ceefe76591ffdeb3ced
guozi007a <1208001055@qq.com>
2023-08-26 12:19:35 +0800

    修改请求路径

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -6,8 +6,7 @@ const About = () => {
 
     useEffect(() => { 
         const aboutMsg = async () => {
-            const res = await request('http://localhost:4001/v1/about')
-            // const res = await request('https://multi-app-blog.fun:9000/v1/about')
+            const res = await request('/v1/about')
             console.log(res)
         }
         aboutMsg()
