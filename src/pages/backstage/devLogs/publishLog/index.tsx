0897e0154c55e23abf8028aaa785adaf9dd67a43
guozi007a <1208001055@qq.com>
2023-09-01 01:43:34 +0800

    发布日志部分功能

--- src/pages/backstage/devLogs/publishLog/index.tsx
+++ src/pages/backstage/devLogs/publishLog/index.tsx
@@ -1,9 +1,27 @@
 /** 后台管理-开发日志-发布日志 */
 import styles from './index.module.scss'
-import { Button, Checkbox, Form, Input, Space } from 'antd';
+import { Button, Checkbox, Form, Input, Space, message } from 'antd';
+import { useState } from 'react';
 
+interface InpItem {
+    key: React.Key
+    id: string
+    content?: React.ReactNode
+}
+
+type FieldType = {
+    confirm?: string
+};
+
+let COUNT = 0
+const initVal: InpItem[] = [{
+    key: `logs${COUNT}`,
+    id: `logs${COUNT}`,
+    content: '',
+}]
 
 const PublishLog = () => {
+    const [inpList, setInpList] = useState<InpItem[]>(initVal)
 
     const onFinish = (values: any) => {
         console.log('Success:', values);
@@ -12,52 +30,80 @@ const PublishLog = () => {
     const onFinishFailed = (errorInfo: any) => {
         console.log('Failed:', errorInfo);
     };
-    
-    type FieldType = {
-        logs?: string;
-        logs1?: string;
-        logs2?: string;
-        logs3?: string;
-        confirm?: string;
-    };
+
+    const handleChange = (changedValues: Record<string, string>, allValues: Record<string, string>) => {
+        console.log('changedValues: ', changedValues)
+        console.log('allValues: ', allValues)
+    }
+
+    const handleInsert = (id: string) => {
+        const isHasEmptyInp = inpList.some(v => !v.content)
+        console.log('isHasEmptyInp: ', isHasEmptyInp)
+        if (isHasEmptyInp) {
+            message.warning('添加失败，有输入框未输入内容！')
+            return
+        }
+        const list: InpItem[] = []
+        for (const v of list) {
+            list.push(v)
+            if (v.id === id) {
+                COUNT++
+                list.push({
+                    key: `logs${COUNT}`,
+                    id: `logs${COUNT}`,
+                    content: '',
+                })
+            }
+        }
+    }
+
+    const handleDelete = (id: string) => {
+        if (inpList.length <= 1) {
+            message.warning('删除失败,请至少保留一个输入框！')
+            return
+        }
+        setInpList(inpList.filter(v => v.id !== id))
+        message.success('删除成功！')
+    }
     
     return <>
         <div className={styles.publish_wrap}>
             <Form
                 name="publishLog"
-                // labelCol={{ span: 24 }}
-                // wrapperCol={{ span: 24 }}
                 style={{ width: '100%' }}
-                initialValues={{ remember: true }}
+                initialValues={{ confirm: true }}
                 onFinish={onFinish}
                 onFinishFailed={onFinishFailed}
                 autoComplete="off"
+                onValuesChange={handleChange}
             >
-                <Form.Item<FieldType>
-                    label="日志内容"
-                    name='logs1'
-                    rules={[{ required: true, message: 'Please input logs!' }]}
-                >
-                    <Space>
-                        <Input />
-                        <Button type='primary'>新增</Button>
-                        <Button type='primary' danger>删除</Button>
-                    </Space>
-                </Form.Item>
-                <Form.Item<FieldType>
-                    label="日志内容"
-                    // name="logs2"
-                    rules={[{ required: true, message: 'Please input logs!' }]}
-                >
-                    <Input />
-                </Form.Item>
-                <Form.Item<FieldType>
-                    label="日志内容"
-                    // name="logs3"
-                    rules={[{ required: true, message: 'Please input logs!' }]}
-                >
-                    <Input />
-                </Form.Item>
+                {
+                    inpList.map((v) => {
+                        return (
+                            <Form.Item
+                                key={v.key}
+                                label="日志内容"
+                                name={v.id}
+                                rules={[{ required: true, message: 'Please input logs!' }]}
+                            >
+                                {/* classNames通过item属性，会为Space中的每一个子元素的wrap添加一个类名 */}
+                                <Space className={styles.space} classNames={{item: styles.space_item}}>
+                                    <Input />
+                                    <Button type='primary'
+                                        onClick={() => {
+                                            handleInsert(v.id)
+                                        }}
+                                    >新增</Button>
+                                    <Button type='primary' danger
+                                        onClick={() => {
+                                            handleDelete(v.id)
+                                        }}
+                                    >删除</Button>
+                                </Space>
+                            </Form.Item>
+                        )
+                    })
+                }
 
                 <Form.Item<FieldType>
                     name="confirm"
