db3540782efb70635a9919ccac431ee85116f43e
guozi007a <1208001055@qq.com>
2023-09-02 11:38:38 +0800

    管理日志功能

--- src/pages/backstage/devLogs/manageLog/index.tsx
+++ src/pages/backstage/devLogs/manageLog/index.tsx
@@ -1,19 +1,16 @@
 /** 管理后台-开发日志-管理日志 */
 import styles from './index.module.scss'
 import type { DatePickerProps } from 'antd';
-import { Layout, DatePicker, Button, Space, Empty, List, Input, message } from 'antd'
+import { Layout, DatePicker, Button, Space, Empty, List, Input, message, Popconfirm } from 'antd'
 import 'dayjs/locale/zh-cn';
 import locale from 'antd/es/date-picker/locale/zh_CN';
 import { InpItem } from '../publishLog';
 import { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
-type OperStatus = 'add' | 'edit' | ''
 
 type PropsConfig = {
     item: InpItem
-    operStatus: OperStatus
-    handleOperStatus: (status: OperStatus) => void
     logList: InpItem[]
     handleLogList: (logs: InpItem[]) => void
     operId: string
@@ -41,8 +38,6 @@ const initLog = (): InpItem[] => {
 
 const ListItem = ({
     item,
-    operStatus,
-    handleOperStatus,
     logList,
     handleLogList,
     operId,
@@ -58,8 +53,6 @@ const ListItem = ({
             return
         }
 
-        handleOperStatus('add')
-        
         const list = [...logList]
         const result: InpItem[] = []
         for (const v of list) {
@@ -71,11 +64,26 @@ const ListItem = ({
             }
         }
         handleLogList(result)
+        setVal('')
+        message.success('新增一条日志，请编辑该日志~')
     }
 
     const handleDelete = (id: string) => {
         const list = logList.filter(v => v.id !== id)
         handleLogList(list)
+        message.success('成功删除一条日志~')
+    }
+
+    const handleEdit = (id: string) => {
+        const isHasEmpty = logList.some((v) => !v.content)
+        if (isHasEmpty) {
+            message.error('存在空输入框，请不要进行编辑操作！')
+            return
+        }
+        handleOperId(id)
+        const o: InpItem = logList.find(v => v.id === id) ?? ({} as InpItem)
+        setVal(o.content as string)
+        message.success('开始编辑日志~')
     }
 
     const handleConfirm = (id: string) => {
@@ -93,12 +101,14 @@ const ListItem = ({
         })
         handleLogList(list)
         handleOperId('')
+        message.success('成功更新一条新的日志~')
     }
 
     const handleCancel = (id: string) => {
         const list = logList.filter(v => v.id !== id)
         handleLogList(list)
         handleOperId('')
+        message.success('成功取消更新日志~')
     }
 
     return <>
@@ -142,7 +152,11 @@ const ListItem = ({
                                     handleDelete(item.id)
                                 }}
                             >删除</Button>
-                            <Button type='primary' className={styles.edit_btn}>编辑</Button>
+                            <Button type='primary' className={styles.edit_btn}
+                                onClick={() => {
+                                    handleEdit(item.id)
+                                }}
+                            >编辑</Button>
                         </Space>
                     </>
             }
@@ -153,7 +167,6 @@ const ListItem = ({
 const ManageLog = () => {
 
     const [logList, setLogList] = useState<InpItem[]>(data)
-    const [operStatus, setOperStatus] = useState<OperStatus>('')
     const [operId, setOperId] = useState<string>('')
 
     const onChange: DatePickerProps['onChange'] = (date, dateString) => {
@@ -162,11 +175,10 @@ const ManageLog = () => {
 
     const handleReset = () => {
         setLogList(data)
+        setOperId('')
+        message.success('日志列表已重置~')
     }
 
-    const handleOperStatus = (status: OperStatus) => {
-        setOperStatus(status)
-    }
 
     const handleLogList = (logs: InpItem[]) => {
         setLogList(logs)
@@ -199,8 +211,6 @@ const ManageLog = () => {
                             dataSource={logList}
                             renderItem={(item) => <ListItem
                                 item={item}
-                                operStatus={operStatus}
-                                handleOperStatus={handleOperStatus}
                                 logList={logList}
                                 handleLogList={handleLogList}
                                 operId={operId}
@@ -214,12 +224,18 @@ const ManageLog = () => {
             </Content>
             {
                 logList && logList.length
-                    ? <Footer className={styles.footer}>
-                        <Space>
-                            <Button type='primary' onClick={handleReset}>重置</Button>
-                            <Button type='primary'>提交更新</Button>
-                        </Space>
-                    </Footer>
+                    ? <>
+                        <Footer className={styles.footer}>
+                            <Space>
+                                <Button type='primary' onClick={handleReset}>重置列表</Button>
+                                <Button type='primary'>清空列表</Button>
+                                <Button type='primary' danger>删除全部日志</Button>
+                            </Space>
+                        </Footer>
+                        <Footer className={`${styles.footer} ${styles.summit_wrap}`}>
+                            <Button type='primary' className={styles.submit}>提交更新</Button>
+                        </Footer>
+                    </>
                     : null
             }
         </Layout>
