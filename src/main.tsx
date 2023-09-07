34186eb1b43a25392dd27c9c416aacf60566b50d
wushijie <wushijie@melot.cn>
2023-09-07 14:17:16 +0800

    修复线上无法加载font字体的bug

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,11 @@ import { BrowserRouter } from 'react-router-dom'
 import '~/assets/root.css'
 import '~/assets/common.css'
 import App from '~/router'
+import { dynamicImportFont } from './utils/dynamicImportFont'
 
 // console.log(import.meta.env.ENV_BASE)
+
+dynamicImportFont(import.meta.env.ENV_BASE)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <BrowserRouter basename={import.meta.env.ENV_BASE}>
