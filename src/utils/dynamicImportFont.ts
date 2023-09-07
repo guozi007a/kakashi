92c178769016e171fc59eea863466ed96669d3f1
wushijie <wushijie@melot.cn>
2023-09-07 14:49:59 +0800

    修复线上无法加载font的bug

--- src/utils/dynamicImportFont.ts
+++
@@ -1,9 +0,0 @@
-/** 动态引入font.css文件，因为发到github pages上，根目录会不同 */
-
-export const dynamicImportFont = (mode: string) => {
-    const head = document.head
-    const linkContainer = document.createElement('link')
-    linkContainer.setAttribute('rel', 'stylesheet')
-    linkContainer.setAttribute('href', `${mode}fontface.css`)
-    head.appendChild(linkContainer)
-}
