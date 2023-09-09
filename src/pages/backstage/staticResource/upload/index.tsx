dc93c83c59cf230bd28cc7682c5877b720d75c4a
guozi007a <1208001055@qq.com>
2023-09-09 09:56:33 +0800

    上传基本结构

--- src/pages/backstage/staticResource/upload/index.tsx
+++ src/pages/backstage/staticResource/upload/index.tsx
@@ -1,48 +1,10 @@
 /** 管理后台-静态资源管理-资源上传 */
 import styles from './index.module.scss'
-import { useState } from 'react';
-import { List, Tabs, Divider, Button, Upload } from 'antd';
-import type { UploadProps } from 'antd';
-import { AndroidOutlined, AppleOutlined, UploadOutlined } from '@ant-design/icons';
-import { supportData, supportType } from './supportConfig';
-import type { UploadFile } from 'antd/es/upload/interface';
+import { List } from 'antd';
+import { supportData } from './supportConfig';
 
 const ResourceUpload = () => {
 
-    const [fileList, setFileList] = useState<UploadFile[]>([
-        {
-            uid: '-1',
-            name: 'xxx.png',
-            status: 'done',
-            url: 'http://www.baidu.com/xxx.png',
-        },
-    ]);
-
-    const handleChange: UploadProps['onChange'] = (info) => {
-        let newFileList = [...info.fileList];
-    
-        // 1. Limit the number of uploaded files
-        // Only to show two recent uploaded files, and old ones will be replaced by the new
-        newFileList = newFileList.slice(-2);
-    
-        // 2. Read from response and show file link
-        newFileList = newFileList.map((file) => {
-            if (file.response) {
-                // Component will show file.url as link
-                file.url = file.response.url;
-            }
-            return file;
-        });
-
-        setFileList(newFileList);
-    };
-
-    const props = {
-        action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
-        onChange: handleChange,
-        multiple: true,
-    };
-    
     return <>
         <List
             className={styles.ant_list}
@@ -56,28 +18,6 @@ const ResourceUpload = () => {
                     </ul>
                 </List.Item>
             )}
-        />
-        <Divider />
-        <Tabs
-            className={styles.tabs}
-            defaultActiveKey="1"
-            items={[AppleOutlined, AndroidOutlined, AppleOutlined].map((Icon, i) => {
-                const id = String(i + 1);
-
-                return {
-                    label: (
-                    <span>
-                        <Icon />
-                        {supportType[i]}
-                    </span>
-                    ),
-                    key: id,
-                    children: <Upload {...props} fileList={fileList}>
-                            <Button icon={<UploadOutlined />}>Upload</Button>
-                        </Upload>
-                    ,
-                };
-            })}
         />
     </>
 }
