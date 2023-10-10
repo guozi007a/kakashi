c59c56921e61882b408b51e039db39f8869b5914
wushijie <wushijie@melot.cn>
2023-10-10 11:47:08 +0800

    设置themeColor

--- src/components/logo/index.tsx
+++ src/components/logo/index.tsx
@@ -1,5 +1,6 @@
 import styles from './index.module.scss'
 import { LogoSvg } from '../LogoSvg'
+import type { FillStyle } from '../LogoSvg'
 
 type PropTypes = {
     linkStyle?: React.CSSProperties
@@ -7,11 +8,11 @@ type PropTypes = {
     textStyle?: React.CSSProperties
 }
 
-export const Logo = ({ linkStyle, svgStyle, textStyle }: PropTypes) => {
+export const Logo = ({ linkStyle, svgStyle, textStyle, fillColor }: PropTypes & FillStyle) => {
     return <>
         <a href={import.meta.env.ENV_BASE} className={styles.logo} title='multi-app-blog' style={linkStyle}>
             <div className={styles.svg} style={svgStyle}>
-                <LogoSvg />
+                <LogoSvg fillColor={fillColor} />
             </div>
             <span className={styles.text} style={textStyle}>multi-app-blog</span>
         </a>
--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -2,14 +2,16 @@
 import styles from './index.module.scss'
 import { Logo } from '~/components/logo'
 
+const themeColor = 'var(--pink)'
+
 const Blog = () => {
     return <>
         <header className={styles.header}>
             <div className={styles.logo_wrap}>
-                <Logo />
+                <Logo fillColor={themeColor} textStyle={{color: themeColor}} />
             </div>
         </header>
-        <main>内容主体</main>
+        <main style={{backgroundColor: themeColor}}>内容主体</main>
         <footer className={styles.footer}>CopyRight ©2023 Author github.guozi007a</footer>
     </>
 }
