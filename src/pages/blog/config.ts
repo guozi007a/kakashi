f30daeddbf955c3f0eae6251db3cb802cfbdd20c
wushijie <wushijie@melot.cn>
2023-10-11 18:14:50 +0800

    blog

---
+++ src/pages/blog/config.ts
@@ -0,0 +1,133 @@
+// 博客属性配置相关
+export const blogCate = [
+    {
+        id: 'html',
+        cate: 'html',
+        count: 0,
+    },
+    {
+        id: 'css',
+        cate: 'css',
+        count: 0,
+    },
+    {
+        id: 'js',
+        cate: 'js',
+        count: 0,
+    },
+    {
+        id: 'tools',
+        cate: '工具插件',
+        count: 0,
+    },
+    {
+        id: 'github',
+        cate: 'github',
+        count: 0,
+    },
+    {
+        id: 'prod&design',
+        cate: '产品设计',
+        count: 0,
+    },
+    {
+        id: 'base-computer',
+        cate: '计算机基础',
+        count: 0,
+    },
+    {
+        id: 'compatibility',
+        cate: '兼容性',
+        count: 0,
+    },
+    {
+        id: 'applet',
+        cate: '小程序',
+        count: 0,
+    },
+    {
+        id: 'study-materials',
+        cate: '学习资源',
+        count: 0,
+    },
+    {
+        id: 'bug',
+        cate: 'bug',
+        count: 0,
+    },
+    {
+        id: 'chat-gpt',
+        cate: 'chatGPT',
+        count: 0,
+    },
+    {
+        id: 'docker',
+        cate: 'Docker',
+        count: 0,
+    },
+    {
+        id: 'echarts',
+        cate: 'Echarts',
+        count: 0,
+    },
+    {
+        id: 'eslint',
+        cate: 'Eslint',
+        count: 0,
+    },
+    {
+        id: 'gin',
+        cate: 'Gin',
+        count: 0,
+    },
+    {
+        id: 'go',
+        cate: 'Golang',
+        count: 0,
+    },
+    {
+        id: 'gorm',
+        cate: 'Gorm',
+        count: 0,
+    },
+    {
+        id: 'mysql',
+        cate: 'MySQL',
+        count: 0,
+    },
+    {
+        id: 'nextjs',
+        cate: 'Nextjs',
+        count: 0,
+    },
+    {
+        id: 'nginx',
+        cate: 'Nginx',
+        count: 0,
+    },
+    {
+        id: 'prettier',
+        cate: 'prettier',
+        count: 0,
+    },
+    {
+        id: 'react',
+        cate: 'React',
+        count: 0,
+    },
+    {
+        id: 'vite',
+        cate: 'Vite',
+        count: 0,
+    },
+    {
+        id: 'svg',
+        cate: 'SVG',
+        count: 0,
+    },
+    {
+        id: 'zustand',
+        cate: 'zustand',
+        count: 0,
+    }
+]
--- src/pages/blog/main/index.tsx
+++ src/pages/blog/main/index.tsx
@@ -2,8 +2,13 @@
 import styles from './index.module.scss'
 import { AppUtils } from '~/utils/common'
 import BlogItem from '../item'
+import { useBlogThemeColorStore } from '~/store/useBlogThemeColorStore'
+import { blogCate } from '../config'
+import Scrollbars from 'react-custom-scrollbars-2'
+import AliIcon from '~/components/aliIcon'
 
 const BlogMain = () => {
+    const themeColor = useBlogThemeColorStore(state => state.blogThemeColor)
 
     return <main className={styles.main}>
         <section className={styles.part1}>
@@ -11,6 +16,7 @@ const BlogMain = () => {
                 <img src={AppUtils.serverImg('temp-avatar.png')} alt="" />
             </section>
             <p className={styles.nick}>guozi007a(小拽)</p>
+            <p className={styles.give_msg} style={{backgroundColor: themeColor}}>建议留言</p>
         </section>
         <section className={styles.part2}>
             <p className={styles.title}>最新文章(15)</p>
@@ -24,7 +30,36 @@ const BlogMain = () => {
                 </ul>
             </section>
         </section>
-        <section className={styles.part3}></section>
+        <section className={styles.part3}>
+            <section className={styles.article_cate}>
+                <section className={styles.cate_text}>
+                    <AliIcon icon='icon-fenlei' />
+                    <span>文章分类</span>
+                </section>
+                <section className={styles.cate_main}>
+                    <Scrollbars autoHide>
+                        <ul className={styles.cate_ul}>
+                            {
+                                blogCate.map((v) => {
+                                    return <li key={v.id} className={styles.cate_li}>
+                                        <p className={styles.cate}>
+                                            <span>{v.cate}</span>
+                                            <span>{v.count}</span>
+                                        </p>
+                                    </li>
+                                })
+                            }
+                        </ul>
+                    </Scrollbars>
+                </section>
+            </section>
+            <section className={styles.corperation}>
+                <section className={styles.corp_text}>
+                    <AliIcon icon='icon-hezuo' />
+                    <span>商务合作</span>
+                </section>
+            </section>
+        </section>
     </main>
 }
 
